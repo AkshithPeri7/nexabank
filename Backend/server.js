37cccc69df156ff5d7dd299de7b209eabb0ad2d5
AkshithPeri7 <akshithperi07@gmail.com>
2026-05-05 17:42:25 +0530

    Final: Add Netlify URL to CORS

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ app.use(cors({
         'http://127.0.0.1:3000',
         'http://localhost:5500',
         'http://127.0.0.1:5500',
-        'https://YOUR_NETLIFY_SITE.netlify.app' // replace after Netlify deploy
+        'https://astounding-croquembouche-70ce0e.netlify.app'
     ],
     credentials: true
 }));
